3f91d0a8e5213b8c7f1056698aef8746733d5a19
Howard Wetsman MD <31905417+hwetsman@users.noreply.github.com>
2022-03-10 10:15:17 -0600

    Refactor: add explanation

--- Text_Summarization.py
+++ Text_Summarization.py
@@ -1,4 +1,5 @@
 """
+Extract the sentence with the most frequently used words from a paragraph
 Based on script by Aman Kharwal
 added 3-10-22
 """
@@ -26,16 +27,18 @@ for word in processed_text:
 
 max_freq = max(word_freq.values())
 for word in word_freq.keys():
-word_freq[word] = (word_freq[word]/max_freq)
+  word_freq[word] = (word_freq[word]/max_freq)
+  
 sent_list = nltk.sent_tokenize(text)
 sent_score = {}
 for sent in sent_list:
-for word in nltk.word_tokenize(sent.lower()):
-if word in word_freq.keys():
-if sent not in sent_score.keys():
-sent_score[sent] = word_freq[word]
-else:
-sent_score[sent] = sent_score[sent] + word_freq[word]
+  for word in nltk.word_tokenize(sent.lower()):
+    if word in word_freq.keys():
+      if sent not in sent_score.keys():
+        sent_score[sent] = word_freq[word]
+      else:
+        sent_score[sent] = sent_score[sent] + word_freq[word]
+        
 summary_sents = nlargest(length, sent_score, key=sent_score.get)
 summary = " ".join(summary_sents)
 print(summary)
